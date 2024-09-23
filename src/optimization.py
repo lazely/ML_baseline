f250793f05f283eb637e8a3422f69dedff40850e
lazely <lz0136@naver.com>
2024-09-23 10:29:24 +0900

    fix : fix tune.py

--- src/optimization.py
+++ src/optimization.py
@@ -4,15 +4,16 @@ from optuna.samplers import TPESampler
 
 from src.utils.params import get_params
 
-def objective(trial):
-    config = get_params(trial=trial)
-    
-    return train.run(config)
+def get_objective(bconfig):
+    def objective(trial):
+        config = get_params(bconfig, trial=trial)
+        return train.run(config)
+    return objective
 
 def run(config):
-    hp_config = config['hyperparameter_optimization']
     study = optuna.create_study(direction="maximize", sampler=TPESampler())
-    study.optimize(objective, n_trials=hp_config['n_trials'])
+    objective = get_objective(config)
+    study.optimize(objective, n_trials=config['hyperparameter_optimization']['n_trials'])
     print("Best trial:")
     trial = study.best_trial
     print("  Value: ", trial.value)
